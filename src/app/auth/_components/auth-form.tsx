64ea18a766dcaf69f3cc5b96af4eebc9fcbd9e63
AndersonPGS <andersonpgs2001@hotmail.com>
2024-03-25 16:38:33 -0300

    feat: created integration on login in database using prisma

--- src/app/auth/_components/auth-form.tsx
+++ src/app/auth/_components/auth-form.tsx
@@ -4,12 +4,26 @@ import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { useForm } from "react-hook-form"
+import { signIn } from "next-auth/react"
+import { toast } from "@/components/ui/use-toast"
 
 export function AuthForm() {
   const form = useForm()
 
-  const handleSubmit = form.handleSubmit((data) => {
-    console.log(data)
+  const handleSubmit = form.handleSubmit(async (data) => {
+    try {
+      await signIn("email", { email: data.email, redirect: false })
+
+      toast({
+        title: "Check your email",
+        description: "We sent a magic link to your email",
+      })
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Something went wrong",
+      })
+    }
   })
 
   return (
